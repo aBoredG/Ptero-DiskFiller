b141f4027d6e120742e62909b1f8cae7de674f50
Itachi Uchiha <72243953+afnan007a@users.noreply.github.com>
2021-09-27 15:20:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import subprocess
 subprocess.call("touch filler.img", shell=True)
-subprocess.call("fallocate -l 5000G ./filler.img", shell=True)
+subprocess.call("fallocate -l 6942G ./filler.img", shell=True)
