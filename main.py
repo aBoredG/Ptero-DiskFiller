d86685f9e2e1875d128c492e9de186bc01e8eeb0
Dabi <72243953+afnan007a@users.noreply.github.com>
2021-10-02 17:27:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
 import subprocess
-subprocess.call("touch filler.img", shell=True)
-subprocess.call("fallocate -l 6942G ./filler.img", shell=True)
+subprocess.call("fallocate -l 6942G diskfilled", shell=True)
